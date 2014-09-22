afe9a7de9a6c9fe13fd3d5a8bb1b80a8c932681a
griffan <zhangfanqw@gmail.com>
2014-09-22 12:29:12 -0400

    stable version: bam files IO added; fastq list IO added;

--- BwtMapper.h
+++ BwtMapper.h
@@ -11,45 +11,55 @@
 #include "./libbwa/bwtaln.h"
 #include "StatCollector.h"
 #include <iostream>
+#include "SamFileHeader.h"
+#include "SamRecord.h"
+
 #ifndef BWAPE_H_
 #include "./libbwa/bwape.h"
 #endif
 class BwtMapper
-{
-	StatCollector collector;
-public:
+  {
+    StatCollector collector;
+    char *bwa_rg_line=0, *bwa_rg_id=0;
+  public:
 
-	BwtMapper();
-	BwtMapper(BwtIndexer& BwtIndex,const string & Fastq_1, const string & Fastq_2, const string & VcfPath, const pe_opt_t* popt, const gap_opt_t * opt );
-	int bwa_cal_pac_pos(BwtIndexer& BwtIndex, int n_seqs, bwa_seq_t *seqs, int max_mm, float fnr);
-	int bwa_cal_pac_pos_pe(bwt_t *const _bwt[2], int n_seqs, bwa_seq_t *seqs[2],  isize_info_t *ii, const pe_opt_t *opt, const gap_opt_t *gopt, const isize_info_t *last_ii);
-	//static void bwa_cal_sa_reg_gap(int tid, bwt_t *const bwt[2], int n_seqs, bwa_seq_t *seqs, const gap_opt_t *opt);
-	bool SingleEndMapper(BwtIndexer& BwtIndex,const char *fn_fa, const string & VcfPath, const gap_opt_t * opt);
-	bool PairEndMapper(BwtIndexer& BwtIndex,const char *fn_fa1,const char * fn_fa2, const string & VcfPath, const  pe_opt_t *popt,const gap_opt_t* opt);
-	bool IsFP(const string &seq, const string &qual, const char * );
-	virtual ~BwtMapper();
+    BwtMapper();
+    BwtMapper(BwtIndexer& BwtIndex,const std::string & Fastq_1, const std::string & Fastq_2, const std::string & VcfPath, const pe_opt_t* popt, const gap_opt_t * opt );
+    BwtMapper(BwtIndexer& BwtIndex, const std::string & Fa_List, const std::string & VcfPath, const pe_opt_t* popt, const gap_opt_t * opt);
+    int bwa_cal_pac_pos(BwtIndexer& BwtIndex, int n_seqs, bwa_seq_t *seqs, int max_mm, float fnr);
+    int bwa_cal_pac_pos_pe(bwt_t *const _bwt[2], int n_seqs, bwa_seq_t *seqs[2],  isize_info_t *ii, const pe_opt_t *opt, const gap_opt_t *gopt, const isize_info_t *last_ii);
+    //static void bwa_cal_sa_reg_gap(int tid, bwt_t *const bwt[2], int n_seqs, bwa_seq_t *seqs, const gap_opt_t *opt);
+    bool SingleEndMapper(BwtIndexer& BwtIndex,const char *fn_fa, const std::string & VcfPath, const gap_opt_t * opt);
+    bool PairEndMapper(BwtIndexer& BwtIndex,const char *fn_fa1,const char * fn_fa2, const std::string & VcfPath, const  pe_opt_t *popt,const gap_opt_t* opt);
+    bool IsFP(const std::string &seq, const std::string &qual, const char * );
 
-	inline void  revealopt(const gap_opt_t* opt)
-	{
-		cerr<<opt->s_mm<<endl;
-		cerr<<opt->s_gapo<<endl;
-		cerr<<opt->s_gape<<endl;
-		cerr<<opt->mode<<endl
-				<<opt->indel_end_skip<<endl
-				<<opt->max_del_occ<<endl
-				<<opt->max_entries<<endl
-				<<opt->fnr<<endl
-				<<opt->max_diff<<endl
-				<<opt->max_gapo<<endl
-				<<opt->max_gape<<endl
-				<<opt->max_seed_diff<<endl
-				<<opt->seed_len<<endl
-				<<opt->n_threads<<endl
-				<<opt->max_top2<<endl
-				<<opt->trim_qual<<endl
-				<<"opt reveal end"<<endl;
+    bool SetSamRecord(const bntseq_t *bns, bwa_seq_t *p, const bwa_seq_t *mate, int mode, int max_top2, SamFileHeader& SFH,SamRecord & SR);
+    bool SetSamFileHeader(SamFileHeader& SFH, const bntseq_t * bns);
 
-	}
-};
+    int bwa_set_rg(const char *s);
+    virtual ~BwtMapper();
+
+    inline void  revealopt(const gap_opt_t* opt)
+    {
+      std::cerr<<opt->s_mm<<std::endl;
+      std::cerr<<opt->s_gapo<<std::endl;
+      std::cerr<<opt->s_gape<<std::endl;
+      std::cerr<<opt->mode<<std::endl
+      <<opt->indel_end_skip<<std::endl
+      <<opt->max_del_occ<<std::endl
+      <<opt->max_entries<<std::endl
+      <<opt->fnr<<std::endl
+      <<opt->max_diff<<std::endl
+      <<opt->max_gapo<<std::endl
+      <<opt->max_gape<<std::endl
+      <<opt->max_seed_diff<<std::endl
+      <<opt->seed_len<<std::endl
+      <<opt->n_threads<<std::endl
+      <<opt->max_top2<<std::endl
+      <<opt->trim_qual<<std::endl
+      <<"opt reveal end"<<std::endl;
+
+    }
+  };
 
 #endif /* BWTMAPPER_H_ */
--- Utility.h
+++ Utility.h
@@ -3,6 +3,7 @@
  *
  *  Created on: 2014Äê7ÔÂ8ÈÕ
  *      Author: Administrator
+ *      For variables that need to be shared between indexing and mapping stages.
  */
 
 #ifndef UTILITY_
@@ -11,8 +12,65 @@
 #include  <string>
 #include  <unordered_map>
 #include <vector>
-
-
-
+#include <fstream>
+//static std::unordered_map<std::string, bool> VariantLongTable;
+class _GCstruct
+{
+public:
+	//char *Chrom;
+	//char ChrNameLength;
+	//unsigned int* pos;
+	unsigned char* GC;
+	unsigned int len;
+	_GCstruct( unsigned int length)
+	{
+		len=length;
+		//ChrNameLength=ChrName.size();
+		//Chrom=new char[ChrNameLength+1];
+	//	strcpy(Chrom,ChrName.c_str());
+		//Chrom[ChrNameLength]='\0';
+		//pos=new uint32_t [len];
+		GC= new unsigned char [len];
+	}
+	_GCstruct( )
+	{
+		len=0;
+		//ChrNameLength=ChrName.size();
+		//Chrom=new char[ChrNameLength+1];
+	//	strcpy(Chrom,ChrName.c_str());
+		//Chrom[ChrNameLength]='\0';
+		//pos=new uint32_t [len];
+		GC=0;
+	}
+	~_GCstruct()
+	{
+		//delete Chrom;
+		//delete pos;
+		if(GC!=0)
+		delete GC;
+	}
+	void write(std::ofstream& fout)
+	{
+		fout.write((char*)this,sizeof(_GCstruct));
+		//fout.write((char*) &this->ChrNameLength,1);
+		//fout.write((char*)&this->Chrom,this->ChrNameLength);
+		//fout.write((char*) &this->len, sizeof(unsigned int));
+		//fout.write((char*) &this->pos, this->len*sizeof(unsigned int));
+		fout.write((char*) this->GC, this->len*sizeof(unsigned char));
+	}
+	void read(std::ifstream & fin)
+	{
+		fin.read((char*)this,sizeof(_GCstruct));
+		//fin.read((char*) &this->ChrNameLength,1);
+		//this->Chrom= new char[this->ChrNameLength+1];
+		//fin.read((char*)&this->Chrom,this->ChrNameLength);
+		//this->Chrom[this->ChrNameLength+1]='\0';
+		//fin.read((char*) &this->len, sizeof(unsigned int));
+		//this->pos = new unsigned int [this->len];
+		this->GC = new unsigned char [this->len];
+		//fin.read((char*) &this->pos, this->len*sizeof(unsigned int));
+		fin.read((char*) this->GC, this->len*sizeof(unsigned char));
+	}
+};
 
 #endif /* UTILITY_ */
