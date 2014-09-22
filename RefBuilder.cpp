b4d2892e28d16d91a03173a79adf25471876a38e
Fan Zhang <fanzhang@wonderland.sph.umich.edu>
2014-09-22 12:29:12 -0400

    stable version: bam files IO added; fastq list IO added;

--- RefBuilder.cpp
+++ RefBuilder.cpp
@@ -7,10 +7,10 @@
 #include "algorithm"
 #include "RefBuilder.h"
 #include "Utility.h"
-#include "./samtools-faidx/faidx.h"
-#include "./libStatGen-master/vcf/VcfFileReader.h"
-#include "./libStatGen-master/vcf/VcfHeader.h"
-#include "./libStatGen-master/vcf/VcfRecord.h"
+#include "./misc/faidx.h"
+#include "VcfFileReader.h"
+#include "VcfHeader.h"
+#include "VcfRecord.h"
 #include <fstream>
 using namespace std;
 
@@ -22,7 +22,7 @@ RefBuilder::RefBuilder()
 
 }
 
-RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap_opt_t* opt)
+RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap_opt_t* opt)//, unordered_map<string,bool>& longRefTable)
 {
 	cerr<<"Initialization of RefBwt..."<<endl;
 		//read in ref.fa and ref.fai
@@ -44,7 +44,9 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 		string SelectedSite=VcfPath+".SelectedSite.vcf";
 		InputFile FoutSelectedSite(SelectedSite.c_str(),"w");
 		string GCpath=VcfPath+".gc";
-		ofstream FGC(GCpath);
+		ofstream FGC(GCpath,ios_base::binary);
+		//FGC.write((char*)opt->num_variant_short,sizeof(int));
+		 //_GCstruct * GCstruct=new _GCstruct [opt->num_variant_long+opt->num_variant_short];
 
 		reader.open(VcfPath.c_str(),header);
 		header.write(&FoutSelectedSite);
@@ -52,10 +54,14 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 		unsigned int nseqs(0);
 		unsigned int nmarker(0);
 		int last_pos=0;
+		string last_chr;
+
+		//int num_so_far=0;
 		while(!reader.isEOF())
 		{
-			if(MaskPath!="Empty"&&nmarker>100000)
+			if(nmarker>=opt->num_variant_short)// for short region
 			{
+				//cerr<<"the nmarker:"<<nmarker<<endl;
 				break;
 			}
 			VcfRecord VcfLine;
@@ -65,7 +71,10 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 				continue;
 
 			string Chrom(VcfLine.getChromStr());
+			if(Chrom=="X"||Chrom=="x"||Chrom=="chrX"||Chrom=="chrx"||Chrom=="Y"||Chrom=="y"||Chrom=="chrY"||Chrom=="chry")
+			continue;
 			int Position=VcfLine.get1BasedPosition();
+			if(Chrom==last_chr&&abs(Position-last_pos)<300) continue;
 			int dummy;
 			sprintf(region, "%s:%d-%d", Chrom.c_str(),Position-opt->flank_len,Position+opt->flank_len);
 			if(MaskPath!="Empty")
@@ -73,7 +82,7 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 			//	cerr<<"region:"<<region<<endl;
 			string MaskSeq(fai_fetch(FastaMask, region, &dummy));
 			size_t n = std::count(MaskSeq.begin(), MaskSeq.end(), 'P');
-			if(double(n)/MaskSeq.size()<0.9||Position-last_pos<300) continue;
+			if(double(n)/MaskSeq.size()<0.9) continue;
 
 				if(!VcfLine.write(&FoutSelectedSite,1))
 				{
@@ -85,30 +94,28 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 			string FetchedSeq(fai_fetch(seq, region, &dummy));
 
 			{//Calculate GC content
-				for(int i=Position-opt->flank_len;i!=Position+opt->flank_len+1;++i)
+				_GCstruct GCstruct(opt->flank_len*2+1);
+				for(unsigned int i=Position-opt->flank_len,t=0;i!=Position+opt->flank_len+1;++i,++t)
 				{
-					sprintf(region, "%s:%d-%d",Chrom.c_str(), i-50,i+50);
+					sprintf(region, "%s:%d-%d",Chrom.c_str(), i-50,i+49);
 					string Window(fai_fetch(seq, region, &dummy));
 					int total=0;
-					for(int j=0;j!=Window.size();++j)
+					for(unsigned int j=0;j!=Window.size();++j)
 					{
 						if(Window[j]=='G'||Window[j]=='C'||Window[j]=='g'||Window[j]=='c')
 							total++;
 					}
-					FGC<<Chrom<<"\t"<<i<<"\t"<<double(total)/101<<endl;
+
+					//strcpy(GCstruct[num_so_far].chrom,Chrom.c_str());//potential overflow
+					//GCstruct.pos[t]=i;
+					GCstruct.GC[t]=total;
+					//FGC.write((char*)&GCstruct,sizeof(GCstruct));
 				}
+				//cerr<<Chrom<<"\t"<<Position<<"\t"<<num_so_far<<endl;
+				//num_so_far++;
+				GCstruct.write(FGC);
 			}
 
-
-			//SeqVec.push_back(FetchedSeq);
-			//SeqVecIdx.push_back(nseqs);
-
-
-			//sprintf(region, "%s:%d@%s", Chrom.c_str(),Position,VcfLine.getAlleles(0));
-			//string Coord(region);
-			//string RefAllele(VcfLine.getAlleles(0));
-			//RefTableIndex.insert ( make_pair(string(region),nseqs));
-			//nseqs++;
 
 			if(DEBUG)
 			{
@@ -126,43 +133,183 @@ RefBuilder::RefBuilder(string VcfPath,string RefPath, string MaskPath, const gap
 				}
 			}
 
-			/*if(VcfLine.getNumAlts()>=1)
-			for(unsigned int i=1;i<VcfLine.getNumAlts()+1;i++)
-			{
-				string AltAllele(VcfLine.getAlleles(i));
-				SeqVec.push_back(FetchedSeq.substr(0,opt->flank_len)+AltAllele+FetchedSeq.substr(opt->flank_len+1,opt->flank_len));// position 500 was replaced by AltAllele
-
-				//SeqVecIdx.push_back(nseqs);
-				sprintf(region, "%s:%d@%s", Chrom.c_str(),Position,AltAllele.c_str());
-
-				string Coord(region);
-				RefTableIndex.insert ( make_pair(Coord,nseqs));
-				nseqs++;
-				//cerr<<"Insert alt  allele at:"<<Coord<<endl;
-				//cout<<Coord<<"\t"<<SeqVec[nseqs-1]<<endl;
-			}
-			else
-			{
-				//cerr<<"Only ref allele at:"<<Coord<<endl;
-			}*/
 			SeqVec.push_back(FetchedSeq.substr(0,opt->flank_len)+string("N")+FetchedSeq.substr(opt->flank_len+1,opt->flank_len));// position 500 was replaced by AltAllele
-			//SeqVec.push_back(FetchedSeq);
 			sprintf(region, "%s:%d@%s/%s", Chrom.c_str(),Position, VcfLine.getRefStr(),VcfLine.getAltStr());
-			//string AltAllele(VcfLine.getAlleles(0));
-			//sprintf(region, "%s%s",region,VcfLine.getRefStr());
-			//if (VcfLine.getNumAlts() >0)
-			//for(unsigned int i=1;i!=VcfLine.getNumAlts();i++)
-			//{
-			//sprintf(region, "%s/%s", region,VcfLine.getAltStr());
-		//	}
-			//string Coord(region);
 			RefTableIndex.insert( make_pair(string(region),nseqs));
 			nseqs++;
 			nmarker++;
-			last_pos++;
+			last_pos=Position;
+			last_chr=Chrom;
 		}
+
+		//cerr<<"******************************************************************"<<endl;
+		//reader.open(VcfPath.c_str(),header);
+		// below is for long ref
+		nmarker=0;
+
+		while(!reader.isEOF())
+			{
+				if(nmarker>=opt->num_variant_long)// for long region
+				{
+					break;
+				}
+				VcfRecord VcfLine;
+				//vector<char> SeqVecIdx;
+				reader.readRecord(VcfLine);
+				if(VcfLine.getNumRefBases()  != 1)// filtering abnormal sites
+					continue;
+
+				string Chrom(VcfLine.getChromStr());
+				if(Chrom=="X"||Chrom=="x"||Chrom=="chrX"||Chrom=="chrx"||Chrom=="Y"||Chrom=="y"||Chrom=="chrY"||Chrom=="chry")
+				continue;
+				int Position=VcfLine.get1BasedPosition();
+				if(Chrom==last_chr&&abs(Position-last_pos)<300) continue;
+				int dummy;
+				sprintf(region, "%s:%d-%d", Chrom.c_str(),Position-opt->flank_len*2,Position+opt->flank_len*2);
+				if(MaskPath!="Empty")
+				{
+				//	cerr<<"region:"<<region<<endl;
+				string MaskSeq(fai_fetch(FastaMask, region, &dummy));
+				size_t n = std::count(MaskSeq.begin(), MaskSeq.end(), 'P');
+				if(double(n)/MaskSeq.size()<0.9) continue;
+
+					if(!VcfLine.write(&FoutSelectedSite,1))
+					{
+						cerr<<"Writing retained sites failed!\n";
+					exit(1);
+					}
+				}
+
+				string FetchedSeq(fai_fetch(seq, region, &dummy));
+
+				{//Calculate GC content
+					_GCstruct GCstruct(opt->flank_len*4+1);
+					for(unsigned int i=Position-opt->flank_len*2,t=0;i!=Position+opt->flank_len*2+1;++i,++t)
+					{
+						sprintf(region, "%s:%d-%d",Chrom.c_str(), i-50,i+49);
+						string Window(fai_fetch(seq, region, &dummy));
+						int total=0;
+						for(unsigned int j=0;j!=Window.size();++j)
+						{
+							if(Window[j]=='G'||Window[j]=='C'||Window[j]=='g'||Window[j]=='c')
+								total++;
+						}
+						//cerr<<Chrom<<"\t"<<i<<"\t"<<total<<endl;
+						//strcpy(GCstruct[num_so_far].chrom,Chrom.c_str());//potential overflow
+						//GCstruct.pos[t]=i;
+						GCstruct.GC[t]=total;
+					}
+					GCstruct.write(FGC);
+				}
+
+				SeqVec.push_back(FetchedSeq.substr(0,opt->flank_len*2)+string("N")+FetchedSeq.substr(opt->flank_len*2+1,opt->flank_len*2));// position 500 was replaced by AltAllele
+				sprintf(region, "%s:%d@%s/%s|L", Chrom.c_str(),Position, VcfLine.getRefStr(),VcfLine.getAltStr());
+				RefTableIndex.insert( make_pair(string(region),nseqs));
+				//sprintf(region, "%s:%d@%s/%s|L", Chrom.c_str(),Position, VcfLine.getRefStr(),VcfLine.getAltStr());
+				//longRefTable.insert(make_pair(string(region),true));
+				//VariantLongTable[string(region)]=1;
+				nseqs++;
+				nmarker++;
+				last_pos=Position;
+				last_chr=Chrom;
+
+			}
+
+
+		// choosing chrX and chrY
+		int Ynmarker=0,Xnmarker=0, chr_flag(-1);
+
+				while(!reader.isEOF())
+					{
+						if(Ynmarker>=100&&Xnmarker>=100)// for long region
+						{
+							break;
+						}
+						VcfRecord VcfLine;
+						//vector<char> SeqVecIdx;
+						reader.readRecord(VcfLine);
+						if(VcfLine.getNumRefBases()  != 1)// filtering abnormal sites
+							continue;
+
+						string Chrom(VcfLine.getChromStr());
+						if(Chrom=="X"||Chrom=="x"||Chrom=="chrX"||Chrom=="chrx")
+						{
+							if(Xnmarker>=100) continue;
+							chr_flag=0;
+						}
+						else if(Chrom=="Y"||Chrom=="y"||Chrom=="chrY"||Chrom=="chry")
+						{
+							if(Ynmarker>=100) continue;
+							chr_flag=1;
+						}
+						else
+							continue;
+
+
+						int Position=VcfLine.get1BasedPosition();
+						if((Chrom==last_chr&&abs(Position-last_pos)<300)||(Position>3000000 && Position<151000000)) continue;
+						int dummy;
+						sprintf(region, "%s:%d-%d", Chrom.c_str(),Position-opt->flank_len*2,Position+opt->flank_len*2);
+						/*if(MaskPath!="Empty")
+						{
+						//	cerr<<"region:"<<region<<endl;
+						string MaskSeq(fai_fetch(FastaMask, region, &dummy));
+						size_t n = std::count(MaskSeq.begin(), MaskSeq.end(), 'P');
+						if(double(n)/MaskSeq.size()<0.9) continue;
+
+							if(!VcfLine.write(&FoutSelectedSite,1))
+							{
+								cerr<<"Writing retained sites failed!\n";
+							exit(1);
+							}
+						}*/
+
+						string FetchedSeq(fai_fetch(seq, region, &dummy));
+
+						{//Calculate GC content
+							_GCstruct GCstruct(opt->flank_len*4+1);
+							for(unsigned int i=Position-opt->flank_len*2,t=0;i!=Position+opt->flank_len*2+1;++i,++t)
+							{
+								sprintf(region, "%s:%d-%d",Chrom.c_str(), i-50,i+49);
+								string Window(fai_fetch(seq, region, &dummy));
+								int total=0;
+								for(unsigned int j=0;j!=Window.size();++j)
+								{
+									if(Window[j]=='G'||Window[j]=='C'||Window[j]=='g'||Window[j]=='c')
+										total++;
+								}
+								//FGC<<Chrom<<"\t"<<i<<"\t"<<total<<endl;
+								//strcpy(GCstruct[num_so_far].chrom,Chrom.c_str());//potential overflow
+								//GCstruct.pos[t]=i;
+								GCstruct.GC[t]=total;
+							}
+							GCstruct.write(FGC);
+						}
+
+						SeqVec.push_back(FetchedSeq.substr(0,opt->flank_len*2)+string("N")+FetchedSeq.substr(opt->flank_len*2+1,opt->flank_len*2));// position 500 was replaced by AltAllele
+						sprintf(region, "%s:%d@%s/%s|L", Chrom.c_str(),Position, VcfLine.getRefStr(),VcfLine.getAltStr());
+						RefTableIndex.insert( make_pair(string(region),nseqs));
+						//sprintf(region, "%s:%d@%s/%s|L", Chrom.c_str(),Position, VcfLine.getRefStr(),VcfLine.getAltStr());
+						//longRefTable.insert(make_pair(string(region),true));
+						//VariantLongTable[string(region)]=1;
+						nseqs++;
+						if(chr_flag==0)
+						Xnmarker++;
+						else
+						Ynmarker++;
+
+						last_pos=Position;
+						last_chr=Chrom;
+
+					}
+		//cerr<<"the total gc sites:"<<num_so_far<<endl;
+		//FGC.write((char*)GCstruct,(opt->num_variant_long*(4*opt->flank_len+1)+opt->num_variant_short*(2*opt->flank_len+1))*sizeof(_GCstruct));
+		//int total=(opt->num_variant_long*(4*opt->flank_len+1)+opt->num_variant_short*(2*opt->flank_len+1))*sizeof(_GCstruct);
+		//FGC.write((char*) &nmarker, sizeof( unsigned int));
 		FGC.close();
 		FoutSelectedSite.ifclose();
+		reader.close();
+
 }
 
 RefBuilder::~RefBuilder()
