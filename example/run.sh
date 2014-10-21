114575a3dc531455cab66c78ea6eea193b49570e
griffan <zhangfanqw@gmail.com>
2014-10-21 18:07:32 -0400

    Update libStatGen

---
+++ example/run.sh
@@ -0,0 +1,2 @@
+../src/FastqA --vcf ./hapmap_3.3.b37.vcf.shuffled.all.vcf --dbsnp ./00-All.vcf.gz --ref ./hs37d5.fa --flank_len 1000 --var_short 1000 --mask ./20141007.all.strict_mask.fasta --bam_in ./test.1k.1000bp.debug.bam --cal_dup  --prefix test.out  --t 4
+
