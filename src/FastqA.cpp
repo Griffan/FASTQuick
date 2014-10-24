b3db9a3324d159b802ba77977bd478c4c34a081e
Fan Zhang <fanzhang@1000g.sph.umich.edu>
2014-10-23 22:59:40 -0400

    Fix bug caused by different bgzf conflict

--- src/FastqA.cpp
+++ src/FastqA.cpp
@@ -15,7 +15,8 @@
 #include "BwtIndexer.h"
 #include "BwtMapper.h"
 #include "../misc/params.h"
-#include "../libmpu/src/mpuTool.h"
+#include "../libmpu/mpuTool.h"
+#include "../misc/Error.h"
 using namespace std;
 
 #define USE_BWT 1
@@ -25,7 +26,9 @@ using namespace std;
 #ifndef MPU_PATH
 #define MPU_PATH "mpuTools"
 #endif
-
+extern void notice(const char*,...);
+extern void warning(const char*,...);
+extern void error(const char*,...);
 std::string Prefix("default");
 int main(int argc, char ** argv)
 {
@@ -43,27 +46,27 @@ int main(int argc, char ** argv)
 	 *
 	 */
 
-	std::string RefPath, VcfPath, MaskPath("Empty"), Fastq_1("Empty"), Fastq_2(
-			"Empty"), BamOut("Empty"), BamIn("Empty"),ReadGroup("default"),DepthDist, SitePileup,FaList("Empty"),DBsnpPath;
+	std::string RefPath("Empty"), VcfPath("Empty"), MaskPath("Empty"), Fastq_1("Empty"), Fastq_2(
+			"Empty"), BamOut("Empty"), BamIn("Empty"),ReadGroup("default"),DepthDist, SitePileup,FaList("Empty"),DBsnpPath("Empty");
 
 	bool loggap(0), /*compread(0),*/ nonstop(0), IL13(0);
 	paramList pl;
 
 	BEGIN_LONG_PARAMS(longParameters) LONG_PARAM_GROUP("Input/Output Files","Input/Output files for the program[Complete Path Recommended]")
-	LONG_STRING_PARAM("vcf",&VcfPath,"Input hapmap or your own variant VCF file name")
-	LONG_STRING_PARAM("dbsnp",&DBsnpPath,"dbSNP VCF file.")
-	LONG_STRING_PARAM("ref",&RefPath,"Reference FASTA file.")
-	LONG_STRING_PARAM("mask",&MaskPath,"Repeat Mask FASTA file.")
-	LONG_STRING_PARAM("fastq_1",&Fastq_1,"Pair end 1 fastq file.")
+	LONG_STRING_PARAM("vcf",&VcfPath,"Input Hapmap or Selected Sites VCF file[Required]")
+	LONG_STRING_PARAM("dbsnp",&DBsnpPath,"dbSNP VCF file[Required]")
+	LONG_STRING_PARAM("ref",&RefPath,"Reference FASTA file[Required]")
+	LONG_STRING_PARAM("mask",&MaskPath,"Repeat Mask FASTA file[Leave empty if using Selected Sites VCF]")
+	LONG_STRING_PARAM("fastq_1",&Fastq_1,"Pair end 1 fastq file[Leave empty if using fq_list or bam_in]")
 	LONG_STRING_PARAM("fastq_2",&Fastq_2,"Pair end 2 fastq file.[Leave empty if using single end]")
-	LONG_STRING_PARAM("bam_out",&BamOut,"Output file prefix")
-	LONG_STRING_PARAM("bam_in",&BamIn,"Input bam file path")
-	LONG_STRING_PARAM("fq_list",&FaList,"Path of input fastq files, tab-delimited, one pair-end files per line(one file per line for single end) ")
-	LONG_STRING_PARAM("prefix",&Prefix,"Prefix of all the statistic files")
+	LONG_STRING_PARAM("bam_out",&BamOut,"Output file prefix[Leave empty if using bam_in]")
+	LONG_STRING_PARAM("bam_in",&BamIn,"Input bam file path[Leave empty if using fq_list or fastq_1]")
+	LONG_STRING_PARAM("fq_list",&FaList,"Path of input fastq files, tab-delimited, one pair-end files per line(one file per line for single end)[Leave empty if using bam_in or fastq_1]")
+	LONG_STRING_PARAM("prefix",&Prefix,"Prefix of all the statistic files[Required]")
 
 
 	//LONG_STRING_PARAM("out",&outf,"Output file prefix")
-	LONG_PARAM_GROUP("Parameters for Reference Sequence ", "Parameters being used to extract reference sequences.")
+	LONG_PARAM_GROUP("Parameters for Reference Sequence ", "Parameters being used to extract reference sequences.[All Required]")
 	// LONG_INT_PARAM("K",&Indexer.RollParam.kmer_size,"kmer size for Rolling Hash filtering")
 	// LONG_INT_PARAM("T",&Indexer.RollParam.thresh,"threshold for filtering specific read")
 	// LONG_INT_PARAM("S",&Indexer.RollParam.read_step_size,"step size for extracting kmer")
@@ -72,7 +75,7 @@ int main(int argc, char ** argv)
 	LONG_INT_PARAM("flank_len",&opt->flank_len,"flanking region length around each marker")
 	LONG_INT_PARAM("flank_long_len",&opt->flank_long_len,"long flanking region length around each marker")
 
-	LONG_PARAM_GROUP("Parameters for Alignment ", "Parameters the are universal for both single end and pair end alignment.")
+	LONG_PARAM_GROUP("Parameters for Alignment ", "Parameters the are universal for both single end and pair end alignment.[Optional]")
 	LONG_DOUBLE_PARAM("n",&opt->fnr,"Max #diff (int) or missing prob under 0.02 error rate [float]")
 	LONG_INT_PARAM("o",&opt->max_gapo,"maximum number or fraction of gap opens")
 	LONG_INT_PARAM("e",&opte,"maximum number of gap extensions, -1 for disabling long gaps [-1]")
@@ -93,7 +96,7 @@ int main(int argc, char ** argv)
 	LONG_PARAM("I",&IL13,"the input is in the Illumina 1.3+ FASTQ-like format")
 	LONG_PARAM("L",&loggap,"log-scaled gap penalty for long deletions")
 
-	LONG_PARAM_GROUP("Parameters for PairEnd ", "Parameters specified for Pair end mapping .")
+	LONG_PARAM_GROUP("Parameters for PairEnd ", "Parameters specified for Pair end mapping.[Optional]")
 	LONG_INT_PARAM("max_isize",&popt->max_isize," maximum insert size")
 	LONG_INT_PARAM("max_occ",&popt->max_occ,"maximum occurrences for one end ")
 	LONG_PARAM("is_sw",&popt->is_sw,"disable Smith-Waterman for the unmapped mate")
@@ -102,7 +105,7 @@ int main(int argc, char ** argv)
 	LONG_DOUBLE_PARAM("ap_prior",&popt->ap_prior,"prior of chimeric rate (lower bound) ")
 	LONG_PARAM("force_isize",&popt->force_isize," disable insert size estimate")
 
-	LONG_PARAM_GROUP("Parameters for Statistics ", "Parameters specified for statistics and summary .")
+	LONG_PARAM_GROUP("Parameters for Statistics ", "Parameters specified for statistics and summary.[Optional]")
 	LONG_PARAM("cal_dup",&opt->cal_dup,"enable the calculation of duplicated reads in depth calculation ")
 	LONG_PARAM("frac_samp",&opt->frac,"specify the downsampling fraction ")
 	//LONG_STRING_PARAM("depth_dist",&DepthDist,"Output file for depth distribution ")
@@ -113,6 +116,21 @@ int main(int argc, char ** argv)
 	pl.Add(new longParams("Available Options", longParameters));
 	pl.Read(argc, argv);
 	pl.Status();
+	if(RefPath=="Empty")
+	{
+		error("--ref is required");
+		exit(1);
+	}
+	if(VcfPath=="Empty")
+	{
+		error("--vcf is required");
+		exit(1);
+	}
+	if(DBsnpPath=="Empty")
+	{
+		error("--dbsnp is required");
+		exit(1);
+	}
 	if(BamOut!="Empty")
 	{
 	opt->out_bam=strdup(BamOut.c_str());
@@ -134,7 +152,7 @@ int main(int argc, char ** argv)
 	}
 	if (IL13)
 	{
-		std::cerr << "Using Illumina 1.3 version quality system..." << std::endl;
+		error( "Using Illumina 1.3 version quality system..." );
 		opt->mode |= BWA_MODE_IL13;
 	}
 	if (loggap)
@@ -153,7 +171,7 @@ int main(int argc, char ** argv)
 		//string BwtPathR = RefPath + ".rbwt";
 		if (stat(BwtPath.c_str(), &sb) != 0) //|| stat(BwtPathR.c_str(), &sb)!=0)
 		{
-			std::cerr << "Index file doesn't exist, building...\n";
+			notice("Index file doesn't exist, building...\n");
 			RefBuilder ArtiRef(VcfPath, RefPath, DBsnpPath, MaskPath, opt);
 					//Indexer.longRefTable);
 			Indexer.BuildIndex(ArtiRef, RefPath, opt);
@@ -168,7 +186,7 @@ int main(int argc, char ** argv)
 		}
 		else //load ref index
 		{
-			std::cerr << "Index file exists, loading...\n";
+			notice("Index file exists, loading...\n");
 			Indexer.LoadIndex(RefPath);
 			if(FaList!="Empty")
 			{
@@ -210,8 +228,8 @@ int main(int argc, char ** argv)
 //		}
 		//tabix -pvcf hapmap_3.3.b37.vcf.shuffled.all.vcf.SelectedSite.vcf
 		// tabix -s 1 -b 2 -e 2 hapmap_3.3.b37.vcf.shuffled.all.vcf.Pileup.gz
-	fprintf(stderr, "[%s] Version: %s", __func__, PACKAGE_VERSION);
-	fprintf(stderr, "\n[%s] Real time: %.3f sec; CPU: %.3f sec\n", __func__,
+	notice("Version: %s\n", PACKAGE_VERSION);
+	notice("Real time: %.3f sec; CPU: %.3f sec\n",
 			realtime() - t_real, cputime());
 	gap_free_opt(opt);
 	free(popt);
