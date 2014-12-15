a0ed6690c6eeabc9255b089383a0176cf2909883
griffan.zhang <zhangfanqw@gmail.com>
2014-12-14 23:53:23 -0500

    1. Update faidx.c 2. Add in async IO for pair end reads 3. Rearrange multi-thread code

--- src/BwtIndexer.h
+++ src/BwtIndexer.h
@@ -16,6 +16,7 @@
 #include <assert.h>
 #include <cmath>
 #include <time.h>
+#include <iostream>
 //Bloom filter parameters
 #define VCF_SITES 10000
 #define WINDOW_SIZE 1000
@@ -23,7 +24,7 @@
 //#define BLOOM_FPP 0.1
 #define PROCESS_RATIO 0.5
 #define READ_STEP_SIZE 1
-
+static bool debug_flag=false;
 struct _RollParam
 {
 	int kmer_size;
@@ -147,8 +148,8 @@ public:
 
 #define min_only(X,Y) ((X) < (Y) ? (X) : (Y))
 
-#define KMER_32 1
-#ifdef KMER_8
+#define KMER_64_BIT 1
+#ifdef KMER_8_BIT
 	unsigned int mask[6]=
 	{	240, //11110000
 		15,//00001111
@@ -157,7 +158,7 @@ public:
 		204,//11001100
 		51}; //00110011
 #endif
-#ifdef KMER_16
+#ifdef KMER_16_BIT
 	unsigned int mask[6] =
 	{	65280, //1111111100000000
 		255,//0000000011111111
@@ -166,9 +167,13 @@ public:
 		12528,//1111000011110000
 		3855}; //0000111100001111
 #endif
-#ifdef KMER_32
+#ifdef KMER_32_BIT
 	unsigned int mask[6] =
 	{ 0xffff0000, 0xffff, 0xff0000ff, 0xffff00, 0xff00ff00, 0xff00ff };
+#endif
+#ifdef KMER_64_BIT
+	uint64_t mask[6] =
+	{ 0xffffffff00000000, 0xffffffff, 0xffff00000000ffff, 0xffffffff0000, 0xffff0000ffff0000, 0xffff0000ffff };
 #endif
 
 	unsigned char * roll_hash_table[6]; //11110000
@@ -194,12 +199,12 @@ public:
 #endif
 	virtual ~BwtIndexer();
 };
-static double QualQuickTable[41] =
+static double QualQuickTable[46] =
 { 1, 0.7943, 0.6310, 0.5012, 0.3981, 0.3162, 0.2512, 0.1995, 0.1585, 0.1259,
 		0.1000, 0.0794, 0.0631, 0.0501, 0.0398, 0.0316, 0.0251, 0.0200, 0.0158,
 		0.0126, 0.0100, 0.0079, 0.0063, 0.0050, 0.0040, 0.0032, 0.0025, 0.0020,
 		0.0016, 0.0013, 0.0010, 0.0008, 0.0006, 0.0005, 0.0004, 0.0003, 0.0003,
-		0.0002, 0.0002, 0.0001, 0.0001 };
+		0.0002, 0.0002, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001 };
 static std::unordered_map<char, char> match_table =
 	{
 	{ 'a', 'T' },
@@ -216,15 +221,70 @@ inline bool BwtIndexer::IsReadHighQ(const ubyte_t *Q, int len)const//check if fi
 	double tmp(1);
 	for (int i = 0; i != 25; ++i)
 	{
+		//std::cerr << int(Q[i]) << "\t";
 		tmp *= (1 - QualQuickTable[Q[i]]);
 	}
+	//std::cerr << std::endl;
 	if (1 - tmp < 0.1)
 		return true;
 	else
 		return false;
 }
 
+inline uint32_t BwtIndexer::KmerShrinkage(uint64_t kmer, unsigned int iter)const
+{
+	//uint32_t tmp(0), i(1);
+	////printf("the incoming kmer:%016llx\t",a);
+	//while (mask) //32 circle
+	//{
+	//	if (mask & 1)
+	//	{
+	//		tmp |= ((a & 3) << (2 * i - 2));
+	//		++i;
+	//	}
 
+	//	mask >>= 1;
+	//	a >>= 2;
+	//}
+	////printf("the resulted i is :%d\n",i);
+	//return tmp;
+
+	uint32_t shrinked(0);
+	switch (iter)
+	{
+	case 0:
+	/*0xffffffff00000000*/
+	shrinked |= (kmer & 0xffffffff00000000) >> 32;
+	return shrinked;
+	case 1:
+	/*0xffffffff*/
+	shrinked |= kmer & 0xffffffff;
+	return shrinked;
+	case 2:
+	/*0xffff00000000ffff*/
+	shrinked |= (kmer & 0xffff000000000000) >> 32;
+	shrinked |= (kmer & 0xffff);
+	return shrinked;
+	case 3:
+	/*0xffffffff0000*/
+	shrinked |= (kmer & 0xffffffff0000) >> 16;
+	return shrinked;
+	case 4:
+	/*0xffff0000ffff0000*/
+	shrinked |= (kmer & 0xffff000000000000) >> 32;
+	shrinked |= (kmer & 0xffff0000) >> 16;
+	return shrinked;
+	case 5:
+	/*0xffff0000ffff*/
+	shrinked |= (kmer & 0xffff00000000) >> 16;
+	shrinked |= (kmer & 0xffff);
+	return shrinked;
+	default:
+		fprintf(stderr,"Fatal error: unknown mask.\n");
+		exit(EXIT_FAILURE);
+	}
+	
+}
 
 inline void BwtIndexer::AddSeq2Hash(const std::string & Seq,
 		const gap_opt_t * opt)
