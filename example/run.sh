6d29bc95d7b12eef7a352f9b948d3c5ad618a4da
griffan <zhangfanqw@gmail.com>
2015-03-05 17:28:44 -0500

    fnished test run

--- example/run.sh
+++ example/run.sh
@@ -1,2 +1,6 @@
-../src/FastqA --vcf ./hapmap_3.3.b37.vcf.shuffled.all.vcf --dbsnp ./00-All.vcf.gz --ref ./hs37d5.fa --flank_len 1000 --var_short 1000 --mask ./20141007.all.strict_mask.fasta --bam_in ./test.1k.1000bp.debug.bam --cal_dup  --prefix test.out  --t 4
-
+../src/FASTQuick index --hapmap hapmap.test.vcf.gz --dbsnp dbsnp.test.vcf.gz --ref test.fa --index_prefix NA12878
+../src/FASTQuick align --fq_list test.fq.list --index_prefix NA12878 --prefix test_out
+if [ $? -ne 0 ]	
+then exit $?;
+	else echo "Test done successfully.";
+	fi
