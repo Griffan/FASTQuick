6a3ff4cee884046b5e712874f16d105e3fe12891
griffan <zhangfanqw@gmail.com>
2015-03-09 18:13:26 -0400

    upload fake test files

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,7 @@
 ../src/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_idx_prefix NA12878
 ../src/FASTQuick align --fq_list test.fq.list --in_idx_prefix NA12878 --out_prefix test_out
 ../src/FASTQuick pop --UD ../resource/hapmap_3.3.b37.dat.UD --PC ../resource/hapmap_3.3.b37.dat.V --mu ../resource/hapmap_3.3.b37.dat.mu --gl fake.likelihood --bed ../resource/choose.bed.post.bed.allele.bed
-../src/FASTQuick con --in_prefix test_out
+../src/FASTQuick con --vcf ../resource/hs37d5.fa.SelectedSite.vcf.gz --mpu fake.Pileup.gz --out test_out --smID default
 if [ $? -ne 0 ]	
 then exit $?;
 	else echo "Test done successfully.";
