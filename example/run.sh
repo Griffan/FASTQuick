35de4c0e959ce109e9cea884c2af2f76eeb7d72c
griffan <zhangfanqw@gmail.com>
2015-03-09 17:06:17 -0400

    fix libmpu compile

--- example/run.sh
+++ example/run.sh
@@ -1,5 +1,7 @@
-../src/FASTQuick index --hapmap hapmap.test.vcf.gz --dbsnp dbsnp.test.vcf.gz --ref test.fa --index_prefix NA12878
-../src/FASTQuick align --fq_list test.fq.list --index_prefix NA12878 --prefix test_out
+../src/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_idx_prefix NA12878
+../src/FASTQuick align --fq_list test.fq.list --in_idx_prefix NA12878 --out_prefix test_out
+../src/FASTQuick pop --UD ../resource/hapmap_3.3.b37.dat.UD --PC ../resource/hapmap_3.3.b37.dat.V --mu ../resource/hapmap_3.3.b37.dat.mu --gl fake.likelihood --bed ../resource/choose.bed.post.bed.allele.bed
+../src/FASTQuick con --in_prefix test_out
 if [ $? -ne 0 ]	
 then exit $?;
 	else echo "Test done successfully.";
