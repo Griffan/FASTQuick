3dc38b1214fb2ad6eced4b6896c97264d78df6ea
Fan Zhang <zhangfanqw@gmail.com>
2016-03-15 12:51:31 -0400

    update testing run example/run.sh

--- example/run.sh
+++ example/run.sh
@@ -1,8 +1,8 @@
-../src/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_idx_prefix NA12878
-../src/FASTQuick align --fq_list test.fq.list --in_idx_prefix NA12878 --out_prefix test_out
-../src/FASTQuick pop --UD ../resource/hapmap_3.3.b37.dat.UD --PC ../resource/hapmap_3.3.b37.dat.V --mu ../resource/hapmap_3.3.b37.dat.mu --gl fake.likelihood --bed ../resource/choose.bed.post.bed.allele.bed
-../src/FASTQuick con --in_idx_prefix NA12878  --in_prefix fake 
+../src/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_index_prefix NA12878
+../src/FASTQuick align --fq_list test.fq.list --in_index_prefix NA12878 --out_prefix test_out
+../src/FASTQuick pop --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed
+../src/FASTQuick con --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed --out test
 if [ $? -ne 0 ]	
 then exit $?;
-	else echo "Test done successfully.";
-	fi
+else echo "Test done successfully.";
+fi
