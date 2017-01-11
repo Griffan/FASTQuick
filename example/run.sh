cb3547f0f507737271220c0927b03d8f7327530e
griffan <zhangfanqw@gmail.com>
2017-01-11 17:13:39 -0500

    add CMakeLists.txt

--- example/run.sh
+++ example/run.sh
@@ -1,7 +1,7 @@
-../src/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_index_prefix NA12878
-../src/FASTQuick align --fq_list test.fq.list --in_index_prefix NA12878 --out_prefix test_out
-../src/FASTQuick pop --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed
-../src/FASTQuick con --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed --out test
+../bin/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref test.fa --out_index_prefix NA12878
+../bin/FASTQuick align --fq_list test.fq.list --in_index_prefix NA12878 --out_prefix test_out
+../bin/FASTQuick pop --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed
+../bin/FASTQuick con --SVD_prefix ../resource/hapmap_3.3.b37.dat --pileup fake.Pileup.gz --BED ../resource/choose.bed --out test
 if [ $? -ne 0 ]	
 then exit $?;
 else echo "Test done successfully.";
