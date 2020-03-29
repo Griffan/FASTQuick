eb4178a64448746ec826863adeda1d84c2867ab6
griffan <griffanzhang2013@gmail.com>
2020-03-29 21:48:35 +0800

    1. update resource files; 2. update tests in example.sh

--- example/run.test.sh
+++ example/run.test.sh
@@ -1,16 +1,23 @@
-../bin/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref ref.test.fa --out_prefix test_out_ref &>test_out.e.log
+# Validate tools exist on path
+for tool in samtools; do
+	if ! which $tool >/dev/null; then
+	  echo "Error: unable to find $tool on \$PATH, please install before continue" 1>&2 ; exit 2; fi
+	echo "Found $(which $tool)" 1>&2
+done
+
+../bin/FASTQuick index --siteVCF hapmap.test.vcf.gz --dbsnpVCF dbsnp.test.vcf.gz --ref ref.test.fa --out_prefix test_out_ref 1>test_out.o.log 2>test_out.e.log
 if [ $? -ne 0 ]
 then exit $?;
 else echo "Test on index finished successfully.";
 fi
-../bin/FASTQuick align --fq_list fq.test.list --index_prefix test_out_ref --out_prefix test_out &>test_out.e.log
+../bin/FASTQuick align --fq_list fq.test.list --index_prefix test_out_ref --out_prefix test_out 1>test_out.o.log 2>test_out.e.log
 if [ $? -ne 0 ]
 then exit $?;
 else echo "Test on align finished successfully.";
 fi
 samtools sort test_out.bam >test_out.sorted.bam
 samtools index test_out.sorted.bam
-../bin/FASTQuick pop+con --DisableSanityCheck --BamFile test_out.sorted.bam --SVDPrefix ../resource/hapmap_3.3.b37.dat --Reference ref.test.fa --Output test_out&>test_out.e.log
+../bin/FASTQuick pop+con --DisableSanityCheck --BamFile test_out.sorted.bam --SVDPrefix ../resource/hapmap_3.3.b37.dat --Reference ref.test.fa --Output test_out 1>test_out.o.log 2>test_out.e.log
 if [ $? -ne 0 ]
 then exit $?;
 else
